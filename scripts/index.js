314746df4b30f13c9ac5de56ca07bab723d4f57f
AdriaanVast <a.vast@live.nl>
2018-08-26 13:58:16 +0200

    Added a query to get venue details and picture. Added a reset for the search. Created a layout for the returned venues.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
 let locationSet = false;
 
+let clientId = 'HZWTV4JI1MKHKHQUYDZ2LEFKKPFJGBQ5S2Y4AVYXV3QKORVJ';
+let clientSecret = 'GK3Z1PCUCG5L2A2NO5NYPXTCHAPE2EAASHNUR03PLUQLKMV3';
+let foursquareVersioning = '20180825';
+
 //On document ready setup all necessary functionality
 $(function () {
     setUserLocation();
 
-    $('.selection-button').on('click', function () {
+    $('.pg-selection-button').on('click', function () {
         chooseOption($(this));
     });
 
     $('#confirm-search-button').on('click', function () {
         if (locationSet) {
-            queryFoursquare();
+            foursquareExploreQuery();
         } else {
             showLocationNotSetWarning()
         }
@@ -19,15 +23,27 @@ $(function () {
     $('#location-input-field').on('keyup', function (event) {
         locationInputKeyupFunction(event);
     });
+
+    $('#reset-search-button').on('click', function () {
+        resetSearch()
+    });
 });
+
+function resetSearch() {
+    $('#reset-search-button').addClass('disabled');
+    $('#selected-items-row').html('');
+    $('.pg-selection').attr('hidden', true);
+    $('.pg-selection-1').attr('hidden', false);
+    $('#location-input-field').val('');
+    if($('#userLocation').val() === ''){
+        locationSet = false;
+    }
+}
+
 
 //Function to handle what happens when the user performs actions while the location input field is in focus.
 function locationInputKeyupFunction(event) {
-    if ($('#location-input-field').val() === '' && $('#userLocation').val() === '') {
-        locationSet = false;
-    } else {
-        locationSet = true;
-    }
+    locationSet = !($('#location-input-field').val() === '' && $('#userLocation').val() === '');
 
     if (event.keyCode === 13) {
         $('#confirm-search-button').trigger('click');
@@ -38,25 +54,28 @@ function locationInputKeyupFunction(event) {
 function chooseOption(button) {
     if (button.hasClass('choice-1')) {
         $('#chosenSection').val(button.attr('name').valueOf() + '=' + button.val());
-        $('.selection-1').prop('hidden', true);
-        $('.selection-2').prop('hidden', false);
-        addToCurrentSelection(button.attr('id'), 'selection-row-1');
+        $('#reset-search-button').removeClass('disabled');
+        $('.pg-selection-1').prop('hidden', true);
+        $('.pg-selection-2').prop('hidden', false);
+        addToCurrentSelection(button.attr('id'));
 
     } else if (button.hasClass('choice-2')) {
         $('#chosenPrice').val(button.attr('name').valueOf() + '=' + button.val());
-        $('.selection-2').prop('hidden', true);
-        $('.selection-3').prop('hidden', false)
-        addToCurrentSelection(button.attr('id'), 'selection-row-2');
+        $('.pg-selection-2').prop('hidden', true);
+        $('.pg-selection-3').prop('hidden', false);
+        addToCurrentSelection(button.attr('id'));
     }
 }
 
 //Adds the selected choice to the 'current selection' section on the page
-function addToCurrentSelection(selectionName, selectionRowId) {
+function addToCurrentSelection(selectionName) {
     $('#selected-items-row').append(
-        '<div class="col-1"> <button type="button" class="btn btn-outline-secondary" data-toggle="tooltip" data-placement="bottom" title="Click to change selection" name="' + selectionRowId + '">' + selectionName + ' </button> </div>'
+        '<div class="col-1">' +
+        '   <button type="button" class="btn btn-outline-secondary disabled">' + selectionName + '</button> ' +
+        '</div>'
     );
     //Calling this function initializes the tooltip to be styled correctly
-    $('[data-toggle="tooltip"]').tooltip();
+    // $('[data-toggle="tooltip"]').tooltip();
 }
 
 //Set the user location in the hidden input field, if no location can be found the error function is called
@@ -82,8 +101,12 @@ function errorGettingLocation() {
     $('#location-input-field').attr('placeholder', 'Please fill in your desired location.');
 }
 
-function queryFoursquare() {
-    let queryURL = buildQueryURL();
+//Queries the details of a specific venue from Foursquare
+function foursquareVenueQuery(venueId) {
+    let baseURL = 'https://api.foursquare.com/v2/venues/';
+    let queryURL = baseURL + venueId;
+    queryURL += '?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=' + foursquareVersioning;
+
     $.ajax({
         type: 'GET',
         dataType: 'jsonp',
@@ -91,37 +114,98 @@ function queryFoursquare() {
         url: queryURL,
         success: function (data) {
             console.log(data);
+            displayVenueInformation(data);
+        }
+    })
+}
+
+//Queries foursquare for multiple venues
+function foursquareExploreQuery() {
+    let queryURL = buildExploreQueryURL();
+    $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        cache: false,
+        url: queryURL,
+        success: function (data) {
             parseResultData(data);
         }
     })
 }
 
-function buildQueryURL() {
-    let clientId = 'HZWTV4JI1MKHKHQUYDZ2LEFKKPFJGBQ5S2Y4AVYXV3QKORVJ';
-    let clientSecret = 'GK3Z1PCUCG5L2A2NO5NYPXTCHAPE2EAASHNUR03PLUQLKMV3';
-    let foursquareVersioning = '20180825';
+//Build the URL for the explore query
+function buildExploreQueryURL() {
     let baseURL = 'https://api.foursquare.com/v2/venues/explore?';
     let result;
-
-    if ($('#location-input-field').val() !== '') {
-        result = baseURL + 'near=' + $('#location-input-field').val();
+    let resultLimit = 9;
+    let locationInputValue = $('#location-input-field').val();
+    if (locationInputValue !== '') {
+        result = baseURL + 'near=' + locationInputValue;
     } else {
-        result = baseURL + 'll=' + $('#userLocation').val();
+        result = baseURL + 'll=' + $('#userLocation').val() + '&radius=5000';
     }
     result = result + '&' + $('#chosenSection').val();
     result = result + '&' + $('#chosenPrice').val();
-    result = result + '&limit=10&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=' + foursquareVersioning;
+    result = result + '&limit=' + resultLimit + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=' + foursquareVersioning;
 
+    console.log(result);
     return result;
 }
 
 //Function to parse the returned json and add it into the DOM for the user to see. Also handles returned error codes.
 function parseResultData(jsonData) {
+    $('#foursquare-results-row').html('');
+    let resultsHeader = $('#foursquare-results-header');
+    resultsHeader.html('');
+    if (jsonData.meta.code !== 200) {
+        resultsHeader.html(
+            '<div class="alert alert-warning alert-dismissible fade show" role="alert">'
+            + jsonData.meta.errorDetail +
+            '   <button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '       <span aria-hidden="true">&times;' +
+            '       </span> ' +
+            '   </button> ' +
+            '</div>'
+        )
+    } else {
+       resultsHeader.html(
+            '<h2>Results found in ' + jsonData.response.headerFullLocation + '</h2>'
+        );
 
+        $.each(jsonData.response.groups[0].items, function (index, value) {
+            foursquareVenueQuery(value.venue.id);
+        })
+    }
 }
 
+//Display a venue in the page using the returned results. Checks if the status code returned is correct.
+function displayVenueInformation(jsonData) {
+    if (jsonData.meta.code === 200) {
+        $('#foursquare-results-row').append(
+            '<div class="col-lg-3 col-md-4 col-sm-6 ml-1 mr-1 mb-2">' +
+            '   <img class="card-img-top pg-card-image" src="' + jsonData.response.venue.bestPhoto.prefix + '600x600' +
+            jsonData.response.venue.bestPhoto.suffix + '" alt="Card image cap">' +
+            '    <div class="card">' +
+            '        <div class="card-body">' +
+            '            <h5 class="card-title">' + jsonData.response.venue.name + '</h5>' +
+            '            <p class="card-text">Category: ' + jsonData.response.venue.categories[0].name + '</p><p></p>' +
+            '            <a href="' + jsonData.response.venue.url + '" class="btn btn-primary" target="_blank">Go to site</a>' +
+            '        </div>' +
+            '    </div>' +
+            '</div>'
+        )
+    }
+}
+
+//Shows an error indicating the user has not set a location yet
 function showLocationNotSetWarning() {
-    $('#alert-display-col').html(
-        '<div class="alert alert-warning alert-dismissible fade show" role="alert"> Location not set, please fill in a location in the textfield below. <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div>'
+    $('#location-alert-display-col').html(
+        '<div class="alert alert-warning alert-dismissible fade show" role="alert"> ' +
+        '   Location not set, please fill in a location in the textfield below. ' +
+        '   <button type="button" class="close" data-dismiss="alert" aria-label="Close"> ' +
+        '       <span aria-hidden="true">&times;' +
+        '       </span> ' +
+        '   </button>' +
+        ' </div>'
     )
 }
